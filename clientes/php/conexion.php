7d3a6e607f2560528d0559107ea7b0c65b266c70
Miguel Marte <40346318+miguelmarte@users.noreply.github.com>
2021-09-28 16:03:05 -0400

    Update conexion.php

--- clientes/php/conexion.php
+++ clientes/php/conexion.php
@@ -1,5 +1,5 @@
 <?php
-    $mysqli=new mysqli('localhost','root','','parking');
+    $mysqli=new mysqli('us-cdbr-east-04.cleardb.com','b03e7f218d9523','247651b0','heroku_94eccb7cf9a3cc6');
     if($mysqli->connect_errno):
         echo "Error al conectarse con MySQL debido al error ".$mysqli->connect_error;
     endif;
